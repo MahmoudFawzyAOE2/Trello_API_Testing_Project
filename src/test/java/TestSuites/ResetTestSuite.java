8e19301b977faab4fec0c766eb82017d885d1afc
Mahmoud Fawzy Taha-Elaraby <1998mft1998@gmail.com>
2024-09-11 22:11:08 +0300

    minor file organization modifications

---
+++ src/test/java/TestSuites/ResetTestSuite.java
@@ -0,0 +1,30 @@
+package TestSuites;
+
+import EndPoints.URLs;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ResetTestSuite {
+
+    @Test(priority = 1)
+    public void createBoard () {
+
+        Response re = given().spec(request)
+                .contentType(ContentType.JSON)
+                .queryParam("name", fakeBoardName)
+                .when().post(URLs.boards)
+                .then().log().all()
+                .assertThat().statusCode(200)
+                .assertThat().contentType(ContentType.JSON)
+                .extract().response();
+
+        Board.setId(re.path("id"));
+        System.out.println(Board.getId());
+
+        Board.setUrl(re.path("url"));
+        System.out.println(Board.getUrl());
+    }
+}
